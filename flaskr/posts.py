38b2fe526e2d7bdc38824bb46bdfcb411bfd77be
Rachel Leona <34417799+rachleona@users.noreply.github.com>
2020-12-23 23:38:09 +0000

    Update posts.py

--- flaskr/posts.py
+++ flaskr/posts.py
@@ -1,4 +1,5 @@
 import functools
+import json
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
@@ -13,84 +14,111 @@ bp = Blueprint('posts', __name__, url_prefix='/posts')
 @bp.route('/<post_id>')
 @login_required
 def viewPost(post_id):
+    db = get_db()
     post = db.execute(
-        'SELECT * FROM posts WHERE id = ?', (post_id,)
+        'SELECT a.id, a.created, a.title, a.body, a.anon, a.edited, a.likes, b.username FROM posts a, users b WHERE a.id = ? AND b.id = a.author', (post_id,)
     ).fetchone()
 
-    comments = db.execute(
-        'SELECT * FROM comments WHERE post = ? ORDER BY created', (post_id,)
-    )
+    if not post:
+        return apology("Page not found", 404)
 
-    return render_template("posts/view.html", post=post, comments=comments)
+    post = dict(zip(['id', 'created', 'title', 'body', 'anon', 'edited', 'likes', 'author'], post))
+
+    comments = db.execute(
+        'SELECT a.created, a.body, a.post, a.anon, a.likes, b.username FROM comments a, users b WHERE a.post = ? AND b.id = a.author ORDER BY created', (post_id,)
+    ).fetchall()
+
+    if comments:
+        comments = dict(zip(['created', 'body', 'post', 'anon', 'likes', 'author'], comments))
+
+    return render_template("posts/view.html", post=post)
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def createPost():
     if request.method == 'POST':
+        db = get_db()
         title = request.form["title"]
         body = request.form["body"]
-        anon = request.form["anon"]
+        anon = False if not request.form.get("anon") else True
 
-        try:
-            db.execute(
-                'INSERT INTO posts (author, title, body, likes, anon) VALUES( ?, ?, ?, ?, ? )',
-                (session.get("user_id"), title, body, [], anon)
-            )
-        except e as error:
-            return apology("Something went wrong! Please try again.", 500, error)
+        print(title, body, anon)
 
-        return redirect(url_for(feed.me))
+        if not db.execute(
+            'INSERT INTO posts (author, title, body, likes, anon) VALUES( ?, ?, ?, ?, ? )',
+            (session.get("user_id"), title, body, json.dumps([]), anon)
+        ):
+            return apology("Something went wrong! Please try again.", 500)
 
-    return render_template("posts/create.html")
+        db.commit()
+
+        return redirect(url_for('feed.myFeed'))
+
+    return render_template("posts/create.html", create=True, post=None)
 
 @bp.route('/edit/<post_id>', methods=('GET', 'POST'))
 @login_required
 def editpost(post_id):
-    post = db.execute(
-        'SELECT * FROM posts WHERE id = ?', (post_id,)
-    ).fetchone()
-
-    if not post['author'] == session.get('user_id'):
-        return apology("Unauthorized access", 403)
-
+    db = get_db()
     if request.method == 'POST':
-        queries = json.load(request.form["queries"])
+        queries = json.loads(request.form["queries"])
         try:
             for q in queries:
+                if q in request.form:
+                    value = request.form[q]
+                else:
+                    value = False
+
                 db.execute(
-                    'UPDATE posts SET ' + q.col + ' = ? WHERE id = ?', (q.val, post_id,)
+                    'UPDATE posts SET ' + q + ' = ? WHERE id = ?', (value, post_id,)
                 )
             db.execute('UPDATE posts SET edited = CURRENT_TIMESTAMP WHERE id = ?', (post_id,))
-        except e as error:
-            return apology("Something went wrong! Please try again.", 500, error)
+        except:
+            return apology("Something went wrong! Please try again.", 500)
             
-        return redirect(url_for(posts[post_id]))
+        db.commit()
+        return redirect('/posts/' + post_id)
 
-    return render_template("posts/create.html", post=post)
+    post = db.execute(
+        'SELECT author, title, body, anon FROM posts WHERE id = ? ', (post_id,)
+    ).fetchone()
 
-@bp.route('/like/<post_id>', methods=('POST'))
+    if post['author'] != session.get('user_id'):
+        return apology("Unauthorized access", 403)
+
+    post = dict(zip(['author', 'title', 'body', 'anon'], post))
+    post['id'] = post_id
+
+    return render_template("posts/create.html", create=False, post=post)
+
+@bp.route('/like/<post_id>', methods=['POST'])
 @login_required
 def likePost(post_id):
-    likes = json.load(request.form["likes"])
+    db = get_db()
+    print(request.form["likes"])
+    likes = json.loads(request.form["likes"])
     user = session.get('user_id')
 
     if user in likes:
-        list.remove(user)
+        likes.remove(user)
     else:
-        list.append(user)
+        likes.append(user)
 
-    try:
-        db.execute(
-            'UPDATE posts SET likes = ? WHERE id = ?', (likes, post_id,)
-        )
-    except e as error:
-        return apology("Something went wrong! Please try again.", 500, error)
+    likes = json.dumps(likes)
 
+    if not db.execute(
+        'UPDATE posts SET likes = ? WHERE id = ?', (likes, post_id,)
+    ):
+        flash("Somethings wrong! Please try again later!")
+        return request.form["likes"]
+
+    db.commit()
     return likes
 
-@bp.route('/delete/<post_id>', methods=('POST'))
+@bp.route('/delete/<post_id>', methods=['POST'])
 @login_required
 def deletePost(post_id):
+    db = get_db()
     author =  db.execute(
         'SELECT author FROM posts WHERE id = ?', (post_id,)
     ).fetchone()['author']
@@ -102,7 +130,9 @@ def deletePost(post_id):
         db.execute(
             'DELETE FROM posts WHERE id = ?', (post_id,)
         )
-    except e as error:
-        return apology("Something went wrong! Please try again.", 500, error)
+    except:
+        return apology("Something went wrong! Please try again.", 500)
 
-    return redirect(url_for(feed.me))
+    db.commit()
+
+    return redirect(url_for('feed.myFeed'))
